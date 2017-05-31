8134c58b05b02f48777bed39854d476b1d1e2a87
Lena Ripple <ripplelena@gmail.com>
2017-05-31 14:16:40 -0600

    name is posting

--- main.go
+++ main.go
@@ -9,8 +9,30 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "index.html")
 }
 
+func items(w http.ResponseWriter, r *http.Request) {
+	// io.WriteString(w, "Hello items!")
+	http.ServeFile(w, r, "items.html")
+}
+
+func newItem(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.NotFound(w, r)
+	}
+	// handle the POST
+	//Call to ParseForm makes form fields available.
+	err := r.ParseForm()
+	if err != nil {
+		// Handle error here via logging and then return
+		http.Error(w, "form parse failed", http.StatusInternalServerError)
+	}
+	name := r.PostFormValue("name")
+	fmt.Fprintf(w, "Hello, %s!", name)
+}
+
 func main() {
 	fmt.Println("hello world")
 	http.HandleFunc("/", hello)
+	http.HandleFunc("/items", items)
+	http.HandleFunc("/new", newItem)
 	http.ListenAndServe(":8000", nil)
 }
